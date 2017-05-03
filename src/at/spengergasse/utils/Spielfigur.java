8bd5d0e71d133713e143799cb12ea79db89e08fe
chris <chris@10.91.71.211>
2017-05-03 15:31:52 +0200

    Move Methode geschrieben

--- src/at/spengergasse/utils/Spielfigur.java
+++ src/at/spengergasse/utils/Spielfigur.java
@@ -4,10 +4,12 @@ public class Spielfigur {
 
 	private String name;
 	private String teamname;
+	private int position;
 
 	public Spielfigur(String name, String teamname) {
 		this.name = name;
 		this.teamname = teamname;
+		setPosition(-1);
 	}
 
 	public String getName(){
@@ -16,11 +18,21 @@ public class Spielfigur {
 	public String getTeamName(){
 		return teamname;
 	}
+	
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
 
 	@Override
 	public String toString() {
-		return "Spielfigur [name=" + name + ", teamname=" + teamname + "]";
+		return "Spielfigur [name=" + name + ", teamname=" + teamname + ", position=" + position + "]";
 	}
+
+	
 	
 	
 }
