6577bc62938679c3ad9886ada9db2013cedee9f4
Raphael <Raphael@10.91.71.196>
2017-05-05 10:46:03 +0200

    Test teilweise angefangen, wird fortgeführt

--- src/at/spengergasse/test/Test.java
+++ src/at/spengergasse/test/Test.java
@@ -1,9 +1,22 @@
 package at.spengergasse.test;
 
+import javax.swing.JFrame;
+
+import at.spengergasse.utils.*;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Team team = null;
+		String eingabe="";
+		Game g = new Game("Team1","Team2","Team3","Team4");
+		JFrame win = new JFrame("Hinweis");
+		g.teamsSetup();
+		win.setTitle("Wählen sie ihr Team.");
+		
+		win.pack();
+		win.setVisible(true);
+		
 		
 
 	}
