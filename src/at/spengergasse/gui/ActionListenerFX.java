b277aabe28030406f5fcda790ddadf2fb354bb8c
chris <chris@10.91.76.0>
2017-05-11 07:57:10 +0200

    Exception im ActionListener ge try n catched

--- src/at/spengergasse/gui/ActionListenerFX.java
+++ src/at/spengergasse/gui/ActionListenerFX.java
@@ -1,15 +1,16 @@
 package at.spengergasse.gui;
+
 import at.spengergasse.utils.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
-public class ActionListenerFX implements EventHandler<ActionEvent>{
-	
+public class ActionListenerFX implements EventHandler<ActionEvent> {
+
 	private final FrameFX gui;
 	private Game game;
 	private final Highscore highscore;
-	
-	public ActionListenerFX(FrameFX frameFX){
+
+	public ActionListenerFX(FrameFX frameFX) {
 		gui = frameFX;
 		highscore = new Highscore();
 	}
@@ -18,41 +19,44 @@ public class ActionListenerFX implements EventHandler<ActionEvent>{
 	public void handle(ActionEvent event) {
 		// TODO Auto-generated method stub
 		Object source = event.getSource();
-		if(source == gui.getStartB()){
+		if (source == gui.getStartB()) {
 			gui.getMainStage().setScene(gui.getGameScene());
 		}
-		if(source == gui.getBack() || source == gui.getBack2() || source == gui.getBack3()){
+		if (source == gui.getBack() || source == gui.getBack2() || source == gui.getBack3()) {
 			gui.getMainStage().setScene(gui.getMainScene());
 		}
-		if(source == gui.getCreditsB()){
+		if (source == gui.getCreditsB()) {
 			gui.getMainStage().setScene(gui.getCreditScene());
 		}
-		if(source == gui.getHighscoreB()){
+		if (source == gui.getHighscoreB()) {
 			gui.getMainStage().setScene(gui.getHighscoreScene());
 			highscore.read();
 			highscore.sort();
-			if(highscore.getHighscore().get(0) != null){
-			gui.getHighscore1name().setText(highscore.getHighscore().get(0).getName());
-			gui.getHighscore1points().setText(""+highscore.getHighscore().get(0).getScore());
-			}
-			if(highscore.getHighscore().get(1) != null){
-			gui.getHighscore2name().setText(highscore.getHighscore().get(1).getName());
-			gui.getHighscore2points().setText(""+highscore.getHighscore().get(1).getScore());
-			}
-			if(highscore.getHighscore().get(2) != null){
-			gui.getHighscore3name().setText(highscore.getHighscore().get(2).getName());
-			gui.getHighscore3points().setText(""+highscore.getHighscore().get(2).getScore());
-			}
-			if(highscore.getHighscore().get(3) != null){
-			gui.getHighscore4name().setText(highscore.getHighscore().get(3).getName());
-			gui.getHighscore4points().setText(""+highscore.getHighscore().get(3).getScore());
-			}
-			if(highscore.getHighscore().get(4) != null){
-			gui.getHighscore5name().setText(highscore.getHighscore().get(4).getName());
-			gui.getHighscore5points().setText(""+highscore.getHighscore().get(4).getScore());
+			try {
+				if (highscore.getHighscore().get(0) != null) {
+					gui.getHighscore1name().setText(highscore.getHighscore().get(0).getName());
+					gui.getHighscore1points().setText("" + highscore.getHighscore().get(0).getScore());
+				}
+				if (highscore.getHighscore().get(1) != null) {
+					gui.getHighscore2name().setText(highscore.getHighscore().get(1).getName());
+					gui.getHighscore2points().setText("" + highscore.getHighscore().get(1).getScore());
+				}
+				if (highscore.getHighscore().get(2) != null) {
+					gui.getHighscore3name().setText(highscore.getHighscore().get(2).getName());
+					gui.getHighscore3points().setText("" + highscore.getHighscore().get(2).getScore());
+				}
+				if (highscore.getHighscore().get(3) != null) {
+					gui.getHighscore4name().setText(highscore.getHighscore().get(3).getName());
+					gui.getHighscore4points().setText("" + highscore.getHighscore().get(3).getScore());
+				}
+				if (highscore.getHighscore().get(4) != null) {
+					gui.getHighscore5name().setText(highscore.getHighscore().get(4).getName());
+					gui.getHighscore5points().setText("" + highscore.getHighscore().get(4).getScore());
+				}
+			} catch (IndexOutOfBoundsException e) {
+
 			}
 		}
 	}
-	
-	
+
 }
--- src/at/spengergasse/test/Test.java
+++ src/at/spengergasse/test/Test.java
@@ -7,15 +7,15 @@ import at.spengergasse.utils.*;
 public class Test {
 
 	public static void main(String[] args) {
-		Team team = null;
+	/*	Team team = null;
 		String eingabe="";
 		Game g = new Game("Team1","Team2","Team3","Team4");
 		JFrame win = new JFrame("Hinweis");
 		g.teamsSetup();
-		win.setTitle("Wählen sie ihr Team.");
+		win.setTitle("Wï¿½hlen sie ihr Team.");
 		
 		win.pack();
-		win.setVisible(true);
+		win.setVisible(true); */
 		
 		
 
